81e58adde0cb9e4182600af7933513138fcc231f
Lucas Zagal <u1051220@umail.utah.edu>
2025-08-22 17:01:52 -0600

    wrote django models, registered them to admin, and added a test post

--- blogPosts/admin.py
+++ blogPosts/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import Tag, BlogPost
 # Register your models here.
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ["word"]
+    
+class BlogPostAdmin(admin.ModelAdmin):
+    list_display = ["title", "publicationDate"]
+
+admin.site.register(Tag, TagAdmin)
+admin.site.register(BlogPost,BlogPostAdmin)
--- blogPosts/models.py
+++ blogPosts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Tag(models.Model):
+    word = models.CharField(max_length=35)
+    
+class BlogPost(models.Model):
+    title = models.CharField(max_length=100)
+    publicationDate = models.DateField()
+    blurb = models.CharField(max_length=250)
+    thumbnail = models.ImageField()
+    contents = models.TextField()
+    tags = models.ManyToManyField(Tag, related_name="posts")
