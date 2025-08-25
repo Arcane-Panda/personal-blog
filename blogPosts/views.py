e1386b68d2d637f10a5f22507f38cf5f555e19f0
Lucas Zagal <u1051220@umail.utah.edu>
2025-08-25 15:39:59 -0600

    main page + aside are now generated from the database

--- blogPosts/views.py
+++ blogPosts/views.py
@@ -12,7 +12,7 @@ def index(request, page_num=0):
     #if there aren't enough for a full chunk, then just render whats left
     all_posts = models.BlogPost.objects.order_by("-publicationDate").all()
     count = all_posts.count()
-    start = 1 + (5*page_num)
+    start =  5*page_num
 
     #if our calculated start is larger than the number of blogposts,
     #we are on a page number that is too high and should return an error
@@ -21,20 +21,32 @@ def index(request, page_num=0):
     
     #eg. page_num =1 should be range 6-10, but if there are only 8 total posts 
     #the range should be 6-8 
-    calculatedEnd = 5 + (5*page_num)
+    calculatedEnd = 4 + (5*page_num)
     end = min(calculatedEnd, count)
     
     main_posts = all_posts[start:end]
+    
     leftover = count - end
     
     #organize archive structure
-    archive = defaultdict(lambda: defaultdict(list))
+    archive_dict = defaultdict(lambda: defaultdict(list))
     for post in all_posts:
         year = post.publicationDate.year
         month = post.publicationDate.strftime("%B")
-        archive[year][month].append(post)
-        
-    return render(request, "index.html", {"main_posts": main_posts,"archive": archive,"pageNum": page_num, "leftover": leftover})
+        archive_dict[year][month].append(post)
+     
+    # Convert to normal dicts and sort
+    archive = {
+        year: {
+            month: posts
+            for month, posts in sorted(months.items(), key=lambda m: m[0], reverse=True)
+        }
+        for year, months in sorted(archive_dict.items(), key=lambda y: y[0], reverse=True)
+    }
+    
+    olderPage = page_num + 1
+    newerPage = page_num - 1
+    return render(request, "index.html", {"main_posts": main_posts,"archive": archive,"pageNum": page_num, "olderPage": olderPage, "newerPage": newerPage , "leftover": leftover})
 
 def bio(request):
     return render(request, "bio.html")
